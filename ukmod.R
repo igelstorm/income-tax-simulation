a1d4ea613b3562fb183d1a25646b86ac4b1a2ac0
Erik Igelström <eigelstrom@gmail.com>
2024-08-21 16:11:28 +0100

    Run UKMOD from R (via RStata)

--- ukmod.R
+++ ukmod.R
@@ -5,64 +5,38 @@ library(RStata)
 options("RStata.StataPath" = "\"C:\\Program Files\\Stata18\\StataMP-64.exe\"")
 options("RStata.StataVersion" = 18)
 
-model_path <- "C:\\Users\\erik\\Documents\\GitHub\\income-tax-simulation\\UKMOD-PUBLIC-B2024.14"
+model_path <- here::here("UKMOD-PUBLIC-B2024.14")
+current_dir <- here::here()
 
 input_data <- fread("UKMOD-PUBLIC-B2024.14/Input/UK_2019_a2.txt")
-input_data
 
-output <- stata(
-  glue(
-    "euromod_run, model(\"{model_path}\") system(UK_2024_MIS) dataset(UK_2019_a2.txt) country(UK) constants(\"MISTaxIncr = '0.05'\")"
+baseline_output <- stata(
+  paste(
+    glue("euromod_run, model(\"{model_path}\") system(UK_2024_MIS) dataset(UK_2019_a2.txt) country(UK)"),
+    glue("cd {current_dir}"),
+    sep = "\n"
   ),
   data.in = input_data,
-  data.out = TRUE
+  data.out = TRUE,
+  stata.echo = TRUE
 )
+
+baseline_output <- as.data.table(baseline_output)
+revenue <- baseline_output[, sum(dwt * (ils_tax + ils_sicee + ils_sicse + ils_sicot + ils_sicer))]
+expenditure <- baseline_output[, sum(dwt * ils_ben)]
+baseline_balance <- revenue - expenditure
+baseline_balance
+
+
+tax_incr <- 0.05
 
 output <- stata(
-  "drop if dgn == 0",
+  paste(
+    glue("euromod_run, model(\"{model_path}\") system(UK_2024_MIS) dataset(UK_2019_a2.txt) country(UK) constants(\"MISTaxIncr = '{tax_incr}'\")"),
+    glue("cd {current_dir}"),
+    sep = "\n"
+  ),
   data.in = input_data,
-  data.out = TRUE
+  data.out = TRUE,
+  stata.echo = TRUE
 )
-
-
-
-euromod_exe <- "C:\\Program Files\\EUROMOD\\Executable\\EM_ExecutableCaller.exe"
-project_dir <- here::here("UKMOD-PUBLIC-B2024.14")
-
-timestamp <- format(Sys.time(), "%Y%m%d%H%M%S")
-input_dir   <- here::here("_ukmod_tmp", timestamp, "input")
-output_dir  <- here::here("_ukmod_tmp", timestamp, "output")
-config_path <- here::here("_ukmod_tmp", timestamp, "config.xml")
-
-
-
-
-input_filename <- "UK_2019_a2.txt"
-input_path <- file.path(project_dir, "Input", input_filename)
-country_file <- "UK.xml"
-
-# dir.create(input_dir, recursive = TRUE)
-dir.create(output_dir, recursive = TRUE)
-
-tictoc::tic()
-sys::exec_wait(
-  euromod_exe,
-  std_out = TRUE,
-  std_err = TRUE,
-  args = c(
-    "-emPath", project_dir,
-    "-sys", "UK_2024",
-    "-data", "UK_2019_a2.txt",
-    "-outPath", output_dir
-  )
-)
-tictoc::toc()
-
-out_files <- list.files(output_dir)
-out_file <- out_files[!grepl("EUROMOD_Log\\.txt$", out_files)]
-
-out_data <- data.table::fread(file.path(output_dir, out_file))
-
-out_data
-
-unlink(here::here("_ukmod_tmp", timestamp), recursive = TRUE)
