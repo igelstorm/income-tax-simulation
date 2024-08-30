84e8d4ca099cd5e6f15729af0d0e8a3ca5c254d5
Erik Igelström <eigelstrom@gmail.com>
2024-08-30 16:15:19 +0100

    More calculations

--- R/summary.R
+++ R/summary.R
@@ -11,33 +11,31 @@ summary <- function(data) {
   data = tar_read(output_baseline)
   setDT(data)
   top_level <- data[, .(
-    govt_revenue = sum(dwt * (ils_tax + ils_sicee + ils_sicse + ils_sicot + ils_sicer)),
+    govt_revenue2 = sum(dwt * (ils_tax + ils_sicdy + ils_sicer)),
     govt_expenditure = sum(dwt * ils_ben)
   )]
 
   by_hh <- data[,.(
     ils_dispy = sum(ils_dispy),
-    ils_origy = sum(ils_origy),
-    weight = sum(dwt),
+    dwt = sum(dwt),
     equiv = 0.67 + 0.33*(sum(dag >= 14) - 1) + 0.2*sum(dag <= 13)
-    # equiv = 1 + 0.5*(sum(dag >= 14) - 1) + 0.3*sum(dag <= 13)
   ), by = "idhh"]
-
-  by_hh[, ils_dispy_equ := ils_dispy / equiv]
-  by_hh[, ils_dispy_equ_qnt := cut_quantile(ils_dispy_equ, q = 5, w = weight)]
-  by_hh[, ils_dispy_equ_dec := cut_quantile(ils_dispy_equ, q = 10, w = weight)]
+  by_hh[, ils_dispy_eq := ils_dispy / equiv]
+  by_hh[, inc_quintile := cut_quantile(ils_dispy_eq, q = 5, w = dwt)]
+  by_hh[, inc_decile := cut_quantile(ils_dispy_eq, q = 10, w = dwt)]
 
   # S80/S20
-  by_hh[ils_dispy_equ_qnt == 5, sum(weight * ils_dispy_equ)] / by_hh[ils_dispy_equ_qnt == 1, sum(weight * ils_dispy_equ)]
+  by_hh[inc_quintile == 5, sum(dwt * ils_dispy_eq)] / by_hh[inc_quintile == 1, sum(dwt * ils_dispy_eq)]
 
   # Gini
-  by_hh[, dineq::gini.wtd(pmax(0, ils_dispy_equ), weight)]
+  by_hh[, dineq::gini.wtd(pmax(0, ils_dispy_eq), dwt)]
 
-  by_hh[, stats::weighted.mean(ils_dispy_equ, weight), by = ils_dispy_equ_dec]
-  by_hh[, mean(ils_dispy_equ)]
-
-  by_hh[, mean(ils_dispy_equ)]
-
-  data[idhh == 5] |> View()
-
+  # Mean income by decile
+  weeks_in_month <- (365 / 12) / 7
+  deciles <- by_hh[, .(
+    mean_inc_eq = stats::weighted.mean(ils_dispy_eq / weeks_in_month, dwt),
+    tot_inc = sum(ils_dispy * dwt)
+  ), by = inc_decile]
+  deciles[, share := 100 * tot_inc / sum(tot_inc)]
+  deciles[order(inc_decile)]
 }
