a688c7a61618feb2b70731fde32825a62ca1ba18
Erik Igelström <eigelstrom@gmail.com>
2024-09-02 13:30:05 +0100

    Merge value and diff columns

--- scripts/tables.R
+++ scripts/tables.R
@@ -2,8 +2,8 @@ library(gt)
 
 data <- list(
   "Baseline" = tar_read(high_level_summary_baseline),
-  "MIS_value" = tar_read(high_level_summary_mis),
-  "Flat_value" = tar_read(high_level_summary_flat)
+  "MIS" = tar_read(high_level_summary_mis),
+  "Flat" = tar_read(high_level_summary_flat)
 )
 
 data |>
@@ -17,15 +17,10 @@ data |>
     "Gini" = gini,
   ) |>
   transpose(keep.names = "Scenario", make.names = "scenario") |>
-  dplyr::mutate(MIS_diff = MIS_value - Baseline, .after = "MIS_value") |>
-  dplyr::mutate(Flat_diff = Flat_value - Baseline, .after = "Flat_value") |>
+  dplyr::mutate(MIS_diff = MIS - Baseline, .after = "MIS") |>
+  dplyr::mutate(Flat_diff = Flat - Baseline, .after = "Flat") |>
   gt() |>
-  tab_spanner_delim("_") |>
-  cols_label(
-    MIS_value = "",
-    Flat_value = "",
-    MIS_diff = "",
-    Flat_diff = "",
-  ) |>
   fmt_number(decimals = 2) |>
-  fmt_number(ends_with("_diff"), decimals = 2, pattern = "({x})", force_sign = TRUE)
+  fmt_number(ends_with("_diff"), decimals = 2, pattern = "({x})", force_sign = TRUE) |>
+  cols_merge(starts_with("MIS")) |>
+  cols_merge(starts_with("Flat"))
