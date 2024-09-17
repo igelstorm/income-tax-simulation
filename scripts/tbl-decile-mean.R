ded02a925d049c8caf64a751a2525dfc536358bf
Erik Igelström <eigelstrom@gmail.com>
2024-09-17 18:07:52 +0100

    Add Denmark to tables

--- scripts/tbl-decile-mean.R
+++ scripts/tbl-decile-mean.R
@@ -5,7 +5,8 @@ library(targets)
 data <- list(
   "Baseline" = tar_read(decile_summary_baseline, store = here::here("_targets")),
   "MIS_value" = tar_read(decile_summary_mis, store = here::here("_targets")),
-  "Flat_value" = tar_read(decile_summary_flat, store = here::here("_targets"))
+  "Flat_value" = tar_read(decile_summary_flat, store = here::here("_targets")),
+  "Denmark_value" = tar_read(decile_summary_dk, store = here::here("_targets"))
 )
 
 data |>
@@ -15,17 +16,21 @@ data |>
     "Income decile" = inc_decile,
     Baseline,
     MIS_value,
-    Flat_value
+    Flat_value,
+    Denmark_value
   ) |>
   dplyr::mutate(MIS_diff = MIS_value - Baseline, .after = "MIS_value") |>
   dplyr::mutate(Flat_diff = Flat_value - Baseline, .after = "Flat_value") |>
+  dplyr::mutate(Denmark_diff = Denmark_value - Baseline, .after = "Denmark_value") |>
   gt() |>
   tab_spanner_delim("_") |>
   cols_label(
     MIS_value = "",
     Flat_value = "",
+    Denmark_value = "",
     MIS_diff = "(change)",
     Flat_diff = "(change)",
+    Denmark_diff = "(change)",
   ) |>
   fmt_number(decimals = 2) |>
   fmt_number(ends_with("_diff"), decimals = 2, pattern = "({x})", force_sign = TRUE) |>
--- scripts/tbl-high-level.R
+++ scripts/tbl-high-level.R
@@ -5,7 +5,8 @@ library(targets)
 data <- list(
   "Baseline" = tar_read(high_level_summary_baseline, store = here::here("_targets")),
   "MIS_value" = tar_read(high_level_summary_mis, store = here::here("_targets")),
-  "Flat_value" = tar_read(high_level_summary_flat, store = here::here("_targets"))
+  "Flat_value" = tar_read(high_level_summary_flat, store = here::here("_targets")),
+  "Denmark_value" = tar_read(high_level_summary_dk, store = here::here("_targets"))
 )
 
 data |>
@@ -21,13 +22,16 @@ data |>
   transpose(keep.names = "Scenario", make.names = "scenario") |>
   dplyr::mutate(MIS_diff = MIS_value - Baseline, .after = "MIS_value") |>
   dplyr::mutate(Flat_diff = Flat_value - Baseline, .after = "Flat_value") |>
+  dplyr::mutate(Denmark_diff = Denmark_value - Baseline, .after = "Denmark_value") |>
   gt() |>
   tab_spanner_delim("_") |>
   cols_label(
     MIS_value = "",
     Flat_value = "",
+    Denmark_value = "",
     MIS_diff = "(change)",
     Flat_diff = "(change)",
+    Denmark_diff = "(change)",
   ) |>
   fmt_number(decimals = 2) |>
   fmt_number(ends_with("_diff"), decimals = 2, pattern = "({x})", force_sign = TRUE) |>
