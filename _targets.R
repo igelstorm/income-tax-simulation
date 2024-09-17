f5db5eea2b1071aceeb3b50afbe6163f786f1340
Erik Igelström <eigelstrom@gmail.com>
2024-09-17 16:47:01 +0100

    Represent scenarios by overriding globals instead of using systems

--- _targets.R
+++ _targets.R
@@ -17,14 +17,34 @@ list(
   )),
   tar_target(output_mis, run_euromod(
     input_data,
-    system = "UK_2024_MIS",
+    system = "UK_2024",
     dataset = "UK_2022_a1.txt",
+    constants = list(
+      ITPerAll = "29500#y",
+      ITRate2 = "0.81",
+      ITRate3 = "0.81",
+      ITRate4S = "0.81",
+      ITRate5S = "0.81",
+      ITRate6S = "0.81"
+    ),
     model_path = model_path
   )),
   tar_target(output_flat, run_euromod(
     input_data,
-    system = "UK_2024_flat",
+    system = "UK_2024",
     dataset = "UK_2022_a1.txt",
+    constants = list(
+      ITPerAll = "0#y",
+      ITRate1 = "0.187",
+      ITRate2 = "0.187",
+      ITRate3 = "0.187",
+      ITRate1S = "0.187",
+      ITRate2S = "0.187",
+      ITRate3S = "0.187",
+      ITRate4S = "0.187",
+      ITRate5S = "0.187",
+      ITRate6S = "0.187"
+    ),
     model_path = model_path
   )),
 
@@ -33,6 +53,7 @@ list(
    tar_target(high_level_summary_flat,      high_level_summary(output_flat)),
 
    tar_target(decile_summary_baseline,      decile_summary(output_baseline)),
+  # TODO: these should use the income deciles boundaries from the baseline scenario
    tar_target(decile_summary_mis,           decile_summary(output_mis)),
    tar_target(decile_summary_flat,          decile_summary(output_flat))
 )
