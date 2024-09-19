0104e512d6b103e2ca24d7582de91b6e52aad104
Erik Igelström <eigelstrom@gmail.com>
2024-09-19 18:34:39 +0100

    Find a Basic tax rate given 0.5 Higher+ rates

--- _targets.R
+++ _targets.R
@@ -61,5 +61,23 @@ list(
     ),
     x_values = c(0.185, 0.187)
   )),
-  tar_target(tri_plot_flat, plot_triangulation(triangulation_flat, xlab = "Tax rate"), packages = "ggpubr")
+  tar_target(triangulation_mis, triangulate_reform(
+    data = input_data, system = "UK_2024", dataset = "UK_2022_a1.txt",
+    model_path = model_path, baseline_data = output_baseline,
+    reform_constants = list(
+      ITPerAll = "29500#y",
+      ITRate1         = "{x}",      # First tax rate
+      ITRate2         = "0.5",      # Second tax rate
+      ITRate3         = "0.5",     # Third tax rate
+      ITRate1S        = "{x}-0.01",     # 2018/19 to current: Starter rate: 2017/18: Basic rate (Scotland)
+      ITRate2S        = "{x}",      # 2018/19 to current: Basic rate; 2017/18: Higher rate (Scotland)
+      ITRate3S        = "{x}+0.01",     # 2018/19 to current: Intermediate rate; 2017/18: Additional rate (Scotland)
+      ITRate4S        = "0.5",     # Higher rate (Scotland)
+      ITRate5S        = "0.5",     # Advanced rate (Scotland)
+      ITRate6S        = "0.5"     # Top rate (Scotland)
+    ),
+    x_values = c(0.40, 0.50)
+  )),
+  tar_target(tri_plot_flat, plot_triangulation(triangulation_flat, xlab = "Tax rate"), packages = "ggpubr"),
+  tar_target(tri_plot_mis, plot_triangulation(triangulation_mis, xlab = "Basic tax rate"), packages = "ggpubr")
 )
