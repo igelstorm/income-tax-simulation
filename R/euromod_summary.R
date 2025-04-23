763aed4c91a5bdf12221fe5c5fbf6d1c834ebc5a
Erik Igelström <eigelstrom@gmail.com>
2025-04-23 13:18:01 +0100

    Only use targets for post-processing of data

---
+++ R/euromod_summary.R
@@ -0,0 +1,62 @@
+cut_quantile <- function(x, q = 5, w = NULL) {
+  cut(
+    x,
+    # Using Hmisc::wtd.quantile here caused occasional edge cases to assign the
+    # quantile NA to the highest and lowest observations, presumably because of
+    # rounding/floating point errors in the 0th and 100th percentiles.
+    # DescTools::Quantile doesn't seem to have this problem.
+    DescTools::Quantile(x, weights = w, probs = 0:q/q),
+    include.lowest = TRUE,
+    labels = 1:q
+  )
+}
+
+create_hh_data <- function(data) {
+  setDT(data)
+  hh_data <- data[,.(
+    ils_dispy = sum(ils_dispy),
+    dwt       = sum(dwt),
+    equiv     = 0.67 + 0.33*(sum(dag >= 14) - 1) + 0.2*sum(dag <= 13)
+  ), by = "idhh"]
+  hh_data[, ils_dispy_eq := ils_dispy / equiv]
+  hh_data
+}
+
+create_hh_deciles <- function(data) {
+  hh_data <- create_hh_data(data)
+  hh_data[, .(
+    idhh,
+    inc_decile = cut_quantile(ils_dispy_eq, q = 10, w = dwt)
+  )]
+}
+
+high_level_summary <- function(data) {
+  hh_data <- create_hh_data(data)
+  hh_data[, inc_quintile := cut_quantile(ils_dispy_eq, q = 5, w = dwt)]
+
+  cbind(
+    data[, .(
+      govt_revenue = sum(dwt * (ils_tax + ils_sicdy + ils_sicer)),
+      govt_expenditure = sum(dwt * ils_ben)
+    )],
+    hh_data[, .(
+      s80s20 = sum(ils_dispy_eq * dwt * (inc_quintile == 5)) / sum(ils_dispy_eq * dwt * (inc_quintile == 1)),
+      gini = dineq::gini.wtd(pmax(0, ils_dispy_eq), dwt)
+    )]
+  )
+}
+
+decile_summary <- function(data, hh_deciles) {
+  hh_data <- create_hh_data(data)
+  hh_data <- merge(hh_data, hh_deciles, by = "idhh")
+
+  weeks_in_month <- (365 / 12) / 7
+
+  deciles <- hh_data[, .(
+    mean_inc_eq = stats::weighted.mean(ils_dispy_eq / weeks_in_month, dwt),
+    tot_inc = sum(ils_dispy * dwt)
+  ), by = inc_decile]
+  deciles[, share := tot_inc / sum(tot_inc)]
+  deciles[, tot_inc := NULL]
+  deciles[order(inc_decile)]
+}
--- _targets.R
+++ _targets.R
@@ -1,100 +1,72 @@
 library(data.table)
 library(targets)
 
-source("R/euromod.R")
-source("R/policy_scenarios.R")
-source("R/summary.R")
-source("R/triangulation.R")
+source("R/euromod_summary.R")
+# source("R/triangulation.R")
 
 model_path <- here::here("UKMOD-PUBLIC-B2024.14")
 input_data_path <- "input/UK_2022_a1.txt"
 input_data_filename <- "UK_2022_a1.txt"
 
 list(
-  tar_target(input_data, fread(input_data_path)),
+  tar_target(euromod_baseline_file,  "intermediate/euromod/baseline/uk_2024_std.txt"),
+  tar_target(euromod_dk_file,        "intermediate/euromod/dk/uk_2024_std.txt"),
+  tar_target(euromod_flat_file,      "intermediate/euromod/flat/uk_2024_std.txt"),
+  tar_target(euromod_mis_file,       "intermediate/euromod/mis/uk_2024_std.txt"),
 
-  tar_target(output_baseline, run_euromod(
-    input_data,
-    system = "UK_2024",
-    dataset = input_data_filename,
-    model_path = model_path
-  )),
-  tar_target(output_mis, run_euromod(
-    input_data,
-    system = "UK_2024",
-    dataset = input_data_filename,
-    constants = mis_constants,
-    model_path = model_path
-  )),
-  tar_target(output_mis2, run_euromod(
-    input_data,
-    system = "UK_2024",
-    dataset = input_data_filename,
-    constants = mis2_constants,
-    model_path = model_path
-  )),
-  tar_target(output_flat, run_euromod(
-    input_data,
-    system = "UK_2024",
-    dataset = input_data_filename,
-    constants = flat_constants,
-    model_path = model_path
-  )),
-  tar_target(output_dk, run_euromod(
-    input_data,
-    system = "UK_2024",
-    dataset = input_data_filename,
-    constants = dk_constants,
-    model_path = model_path
-  )),
+  tar_target(euromod_baseline,   fread(euromod_baseline_file)),
+  tar_target(euromod_dk,         fread(euromod_dk_file)),
+  tar_target(euromod_flat,       fread(euromod_flat_file)),
+  tar_target(euromod_mis,        fread(euromod_mis_file)),
 
-  tar_target(high_level_summary_baseline,  high_level_summary(output_baseline)),
-  tar_target(high_level_summary_mis,       high_level_summary(output_mis2)),
-  tar_target(high_level_summary_flat,      high_level_summary(output_flat)),
-  tar_target(high_level_summary_dk,        high_level_summary(output_dk)),
+  tar_target(high_level_summary_baseline,  high_level_summary(euromod_baseline)),
+  tar_target(high_level_summary_mis,       high_level_summary(euromod_mis)),
+  tar_target(high_level_summary_flat,      high_level_summary(euromod_flat)),
+  tar_target(high_level_summary_dk,        high_level_summary(euromod_dk)),
 
-  tar_target(hh_deciles_baseline, create_hh_deciles(output_baseline)),
+  tar_target(hh_deciles_baseline, create_hh_deciles(euromod_baseline)),
 
-  tar_target(decile_summary_baseline,      decile_summary(output_baseline, hh_deciles = hh_deciles_baseline)),
-  tar_target(decile_summary_mis,           decile_summary(output_mis2, hh_deciles = hh_deciles_baseline)),
-  tar_target(decile_summary_flat,          decile_summary(output_flat, hh_deciles = hh_deciles_baseline)),
-  tar_target(decile_summary_dk,            decile_summary(output_dk, hh_deciles = hh_deciles_baseline)),
+  tar_target(decile_summary_baseline,      decile_summary(euromod_baseline, hh_deciles = hh_deciles_baseline)),
+  tar_target(decile_summary_mis,           decile_summary(euromod_mis, hh_deciles = hh_deciles_baseline)),
+  tar_target(decile_summary_flat,          decile_summary(euromod_flat, hh_deciles = hh_deciles_baseline)),
+  tar_target(decile_summary_dk,            decile_summary(euromod_dk, hh_deciles = hh_deciles_baseline)),
 
-  tar_target(triangulation_flat, triangulate_reform(
-    data = input_data, system = "UK_2024", dataset = input_data_filename,
-    model_path = model_path, baseline_data = output_baseline,
-    reform_constants = list(
-      ITPerAll = "0#y",
-      ITRate1 = "{x}", ITRate2 = "{x}", ITRate3 = "{x}",
-      ITRate1S = "{x}", ITRate2S = "{x}", ITRate3S = "{x}",
-      ITRate4S = "{x}", ITRate5S = "{x}", ITRate6S = "{x}"
-    ),
-    x_values = c(0.185, 0.187)
-  )),
-  tar_target(triangulation_mis, triangulate_reform(
-    data = input_data, system = "UK_2024", dataset = input_data_filename,
-    model_path = model_path, baseline_data = output_baseline,
-    reform_constants = list(
-      ITPerAll = "29500#y",
-      ITRate1         = "{x}",      # First tax rate
-      ITRate2         = "0.5",      # Second tax rate
-      ITRate3         = "0.5",     # Third tax rate
-      ITRate1S        = "{x}-0.01",     # 2018/19 to current: Starter rate: 2017/18: Basic rate (Scotland)
-      ITRate2S        = "{x}",      # 2018/19 to current: Basic rate; 2017/18: Higher rate (Scotland)
-      ITRate3S        = "{x}+0.01",     # 2018/19 to current: Intermediate rate; 2017/18: Additional rate (Scotland)
-      ITRate4S        = "0.5",     # Higher rate (Scotland)
-      ITRate5S        = "0.5",     # Advanced rate (Scotland)
-      ITRate6S        = "0.5"     # Top rate (Scotland)
-    ),
-    x_values = c(0.40, 0.50)
-  )),
-  tar_target(triangulation_dk, triangulate_reform(
-    data = input_data, system = "UK_2024", dataset = input_data_filename,
-    model_path = model_path, baseline_data = output_baseline,
-    reform_constants = dk_raw_params,
-    x_values = c(3.39, 3.395)
-  )),
-  tar_target(tri_plot_flat, plot_triangulation(triangulation_flat, xlab = "Tax rate"), packages = "ggpubr"),
-  tar_target(tri_plot_mis, plot_triangulation(triangulation_mis, xlab = "Basic tax rate"), packages = "ggpubr"),
-  tar_target(tri_plot_dk, plot_triangulation(triangulation_dk, xlab = "UC increase factor"), packages = "ggpubr")
+  # tar_target(triangulation_flat, triangulate_reform(
+  #   data = input_data, system = "UK_2024", dataset = input_data_filename,
+  #   model_path = model_path, baseline_data = euromod_baseline,
+  #   reform_constants = list(
+  #     ITPerAll = "0#y",
+  #     ITRate1 = "{x}", ITRate2 = "{x}", ITRate3 = "{x}",
+  #     ITRate1S = "{x}", ITRate2S = "{x}", ITRate3S = "{x}",
+  #     ITRate4S = "{x}", ITRate5S = "{x}", ITRate6S = "{x}"
+  #   ),
+  #   x_values = c(0.185, 0.187)
+  # )),
+  # tar_target(triangulation_mis, triangulate_reform(
+  #   data = input_data, system = "UK_2024", dataset = input_data_filename,
+  #   model_path = model_path, baseline_data = euromod_baseline,
+  #   reform_constants = list(
+  #     ITPerAll = "29500#y",
+  #     ITRate1         = "{x}",      # First tax rate
+  #     ITRate2         = "0.5",      # Second tax rate
+  #     ITRate3         = "0.5",     # Third tax rate
+  #     ITRate1S        = "{x}-0.01",     # 2018/19 to current: Starter rate: 2017/18: Basic rate (Scotland)
+  #     ITRate2S        = "{x}",      # 2018/19 to current: Basic rate; 2017/18: Higher rate (Scotland)
+  #     ITRate3S        = "{x}+0.01",     # 2018/19 to current: Intermediate rate; 2017/18: Additional rate (Scotland)
+  #     ITRate4S        = "0.5",     # Higher rate (Scotland)
+  #     ITRate5S        = "0.5",     # Advanced rate (Scotland)
+  #     ITRate6S        = "0.5"     # Top rate (Scotland)
+  #   ),
+  #   x_values = c(0.40, 0.50)
+  # )),
+  # tar_target(triangulation_dk, triangulate_reform(
+  #   data = input_data, system = "UK_2024", dataset = input_data_filename,
+  #   model_path = model_path, baseline_data = euromod_baseline,
+  #   reform_constants = dk_raw_params,
+  #   x_values = c(3.39, 3.395)
+  # )),
+  # tar_target(tri_plot_flat, plot_triangulation(triangulation_flat, xlab = "Tax rate"), packages = "ggpubr"),
+  # tar_target(tri_plot_mis, plot_triangulation(triangulation_mis, xlab = "Basic tax rate"), packages = "ggpubr"),
+  # tar_target(tri_plot_dk, plot_triangulation(triangulation_dk, xlab = "UC increase factor"), packages = "ggpubr")
+  tar_target(end, "end")
 )
