64a6cc68bb62745e83a412ad385c950c9b75bfbd
Erik Igelström <eigelstrom@gmail.com>
2025-04-24 14:15:10 +0100

    Use paired differences and t-distribution to calculate confidence intervals

--- figures/confidence-intervals.R
+++ figures/confidence-intervals.R
@@ -1,35 +1,43 @@
 library(targets)
 
 int_scenario <- "dk"
-variable <- "mh_case"
+varname <- "rii"
 
-# Baseline and intervention data
 bln <- tar_read(simpaths_summary_baseline, store = here::here("_targets"))
 int <- tar_read_raw(paste0("simpaths_summary_", int_scenario), store = here::here("_targets"))
 
-bln_values <- bln[strata == "population" & time == 2026, get(variable)]
-int_values <- int[strata == "population" & time == 2026, get(variable)]
-diffs <- sample(int_values, 1000, replace = TRUE) - sample(bln_values, 1000, replace = TRUE)
+bln_values <- bln[strata == "population" & time == 2026, get(varname)]
+int_values <- int[strata == "population" & time == 2026, get(varname)]
+diffs <- int_values - bln_values
+
+bln_mean <- mean(bln_values)
+int_mean <- mean(int_values)
+bln_sd <- sd(bln_values)
+int_sd <- sd(int_values)
+diff_mean <- mean(diffs)
+diff_sd <- sd(diffs)
+
+n <- length(diffs)
 
 paste(
   sprintf("Intervention scenario: %s", int_scenario),
   sprintf(
     "Baseline value:      %.3f (%.3f, %.3f)",
-    median(bln_values, na.rm = TRUE),
-    quantile(bln_values, 0.025, na.rm = TRUE),
-    quantile(bln_values, 0.975, na.rm = TRUE)
+    bln_mean,
+    bln_mean + qt(0.025, df = n - 1) * bln_sd,
+    bln_mean + qt(0.975, df = n - 1) * bln_sd
   ),
   sprintf(
     "Intervention value:  %.3f (%.3f, %.3f)",
-    median(int_values, na.rm = TRUE),
-    quantile(int_values, 0.025, na.rm = TRUE),
-    quantile(int_values, 0.975, na.rm = TRUE)
+    int_mean,
+    int_mean + qt(0.025, df = n - 1) * int_sd,
+    int_mean + qt(0.975, df = n - 1) * int_sd
   ),
   sprintf(
     "Difference:          %.3f (%.3f, %.3f)",
-    median(diffs, na.rm = TRUE),
-    quantile(diffs, 0.025, na.rm = TRUE),
-    quantile(diffs, 0.975, na.rm = TRUE)
+    diff_mean,
+    diff_mean + qt(0.025, df = n - 1) * diff_sd,
+    diff_mean + qt(0.975, df = n - 1) * diff_sd
   ),
   sep = "\n"
 ) |>
