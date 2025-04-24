962698122cb9f933da45f2cbcb16211f366befc1
Erik Igelström <eigelstrom@gmail.com>
2025-04-24 14:35:50 +0100

    Use paired differences and t distribution to calculate effects

--- R/intervention_effects.R
+++ R/intervention_effects.R
@@ -2,32 +2,29 @@ calculate_difference <- function(baseline_data, intervention_data) {
   bln <- baseline_data
   int <- intervention_data
 
-  idcols = c("time", "strata", "inc_decile", "dgn", "deh_c3", "age_cat")
-  bln_mean <- bln[, lapply(.SD, mean), by = idcols] |>
-    melt(id.vars = idcols, variable.name = "variable", value.name = "bln_mean")
-  bln_sd <- bln[, lapply(.SD, sd), by = idcols] |>
-    melt(id.vars = idcols, variable.name = "variable", value.name = "bln_sd")
-  int_mean <- int[, lapply(.SD, mean), by = idcols] |>
-    melt(id.vars = idcols, variable.name = "variable", value.name = "int_mean")
-  int_sd <- int[, lapply(.SD, sd), by = idcols] |>
-    melt(id.vars = idcols, variable.name = "variable", value.name = "int_sd")
+  id_cols = c("time", "strata", "inc_decile", "dgn", "deh_c3", "age_cat")
+  val_cols <- setdiff(names(bln), id_cols)
 
-  data <- bln_mean |>
-    merge(bln_sd) |>
-    merge(int_mean) |>
-    merge(int_sd) |>
-    na.omit(c("bln_mean", "int_mean"))
+  diffs <- bln[, .SD, .SDcols = id_cols]
+  for (col in val_cols) {
+    diffs[, (col) := int[[col]] - bln[[col]]]
+  }
+
+  runs <- length(unique(bln$run))
+
+  diff_mean <- diffs[, lapply(.SD, mean), by = id_cols] |>
+    melt(id.vars = id_cols, variable.name = "variable", value.name = "diff_mean")
+  diff_sd <- diffs[, lapply(.SD, sd), by = id_cols] |>
+    melt(id.vars = id_cols, variable.name = "variable", value.name = "diff_sd")
+
+  data <- merge(diff_mean, diff_sd) |>
+    na.omit("diff_mean")
 
   data[, {
-    bln_values <- rnorm(1000, bln_mean, bln_sd)
-    int_values <- rnorm(1000, int_mean, int_sd)
-    diffs <- int_values - bln_values
-    t_test <- t.test(diffs)
     list(
-      diff = median(diffs, na.rm = TRUE),
-      diff_lower = quantile(diffs, 0.025, na.rm = TRUE),
-      diff_upper = quantile(diffs, 0.975, na.rm = TRUE),
-      diff_pval = t_test$p.value
+      diff = diff_mean,
+      diff_lower = diff_mean + qt(0.025, df = runs - 1) * diff_sd,
+      diff_upper = diff_mean + qt(0.975, df = runs - 1) * diff_sd
     )
-  }, by = c(idcols, "variable")]
+  }, by = c(id_cols, "variable")]
 }
