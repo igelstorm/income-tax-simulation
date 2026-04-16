5966ad72fb02ff06d5bdad9ffd9fa7c22c5ab7db
Erik Igelström <eigelstrom@gmail.com>
2026-04-16 11:07:33 +0100

    Update resource usage and notifications

--- slurm/02_run_simpaths.sh
+++ slurm/02_run_simpaths.sh
@@ -5,11 +5,13 @@
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --error=logs/%x-%j.err
 #SBATCH --partition=nodes
-#SBATCH --mem=16G
+#SBATCH --mem=4G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
+#SBATCH --mail-user=erik.igelstrom@glasgow.ac.uk
+#SBATCH --mail-type=ALL
 
 module load apps/R/4.4.1/gcc-8.5.0+openblas-0.3.28
 module load apps/java/23.0.1/noarch
