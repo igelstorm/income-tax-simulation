5c35fb42973243a69b5831d592b8e046af836785
Erik Igelström <eigelstrom@gmail.com>
2026-04-21 10:28:56 +0100

    Increase Java max heap size

--- slurm/02_run_simpaths.sh
+++ slurm/02_run_simpaths.sh
@@ -17,4 +17,6 @@
 module load apps/R/4.4.1/gcc-8.5.0+openblas-0.3.28
 module load apps/java/23.0.1/noarch
 
+export JAVA_TOOL_OPTIONS="-Xmx3g -XX:+ExitOnOutOfMemoryError"
+
 Rscript scripts/02_run_simpaths.R baseline
