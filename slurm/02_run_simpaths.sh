da51efa7a52f9b5038c75cd086814f7712cff154
Erik Igelström <eigelstrom@gmail.com>
2026-04-22 12:30:28 +0100

    Run all scenarios with Slurm array

--- slurm/02_run_simpaths.sh
+++ slurm/02_run_simpaths.sh
@@ -13,10 +13,12 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mail-user=erik.igelstrom@glasgow.ac.uk
 #SBATCH --mail-type=ALL
+#SBATCH --array=0-3
 
 module load apps/R/4.4.1/gcc-8.5.0+openblas-0.3.28
 module load apps/java/23.0.1/noarch
 
 export JAVA_TOOL_OPTIONS="-Xmx6g -XX:+ExitOnOutOfMemoryError"
+ARGS=(baseline dk mis flat)
 
-Rscript scripts/02_run_simpaths.R baseline
+Rscript scripts/02_run_simpaths.R ${ARGS[$SLURM_ARRAY_TASK_ID]}
